ecaed4f9d15bc045a167ded81489814a68f52a79
Mattia Bonardi <72151183+mattia-bonardi-smeup@users.noreply.github.com>
2022-03-31 21:01:36 +0200

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./configurations/configuration.js";
 export * from "./exceptions/SendEmailException.js";
+export * from "./exceptions/EmailSenderException.js";
+export * from "./exceptions/TemplateNotFoundException.js";
 export * from "./functions/sendEmail.js";
 export * from "./services/loadTemplate.js";
 export * from "./services/resolveTemplate.js";
